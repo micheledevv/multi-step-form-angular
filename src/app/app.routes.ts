32a07f82faa993ea4e263fda79efcd1131d4fa28
michele.pugliese <michele.pugliese@lutech.it>
2025-05-31 14:12:07 +0200

    add lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,26 @@
 import { Routes } from '@angular/router';
-import { StepInfoComponent } from './step-info/step-info.component';
-import { StepPlanComponent } from './step-plan/step-plan.component';
-import { StepAddOnsComponent } from './step-add-ons/step-add-ons.component';
-import { StepSummaryComponent } from './step-summary/step-summary.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'info', pathMatch: 'full' },
-  { path: 'info', component: StepInfoComponent },
-  { path: 'plan', component: StepPlanComponent },
-  { path: 'addons', component: StepAddOnsComponent },
-  { path: 'summary', component: StepSummaryComponent }
+  {
+    path: 'info',
+    loadComponent: () =>
+      import('./step-info/step-info.component').then(m => m.StepInfoComponent),
+  },
+  {
+    path: 'plan',
+    loadComponent: () =>
+      import('./step-plan/step-plan.component').then(m => m.StepPlanComponent),
+  },
+  {
+    path: 'addons',
+    loadComponent: () =>
+      import('./step-add-ons/step-add-ons.component').then(m => m.StepAddOnsComponent),
+  },
+  {
+    path: 'summary',
+    loadComponent: () =>
+      import('./step-summary/step-summary.component').then(m => m.StepSummaryComponent),
+  }
 ];
+
